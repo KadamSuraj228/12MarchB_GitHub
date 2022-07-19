1d603be0db96ea37e9adc9665c8ecabc135bfae6
BusinessComputers.in <BusinessComputers.in@DESKTOP-V2PIM7G>
2022-07-19 17:12:01 +0530

    [Suraj]: Added new project

--- src/Sample1/Demo1.java
+++ src/Sample1/Demo1.java
@@ -1,5 +1,16 @@
 package Sample1;
 
-public class Demo1 {
+public class Demo1 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Hiii");
+		
+		System.out.println("Hiii");
+		
+		System.out.println("Hello");
+		
+		System.out.println("hello");
+	}
 
 }
--- src/Sample1/Demo2.java
+++ src/Sample1/Demo2.java
@@ -1,5 +1,12 @@
 package Sample1;
 
-public class Demo2 {
+public class Demo2 
+{
+	public static void main(String[] args) 
+	{
+		System.out.println("Suraj");
+		
+		System.out.println("Kadam");
+	}
 
 }
